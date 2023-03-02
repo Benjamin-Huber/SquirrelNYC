655c68de339de1c0c1854eaf3bbcd0393529c564
Benjamin Huber <benjamin.huber@nyspi.columbia.edu>
2023-03-02 17:30:42 -0500

    Add Derived Variables and Merge

--- Analysis/DataCleaning.R
+++ Analysis/DataCleaning.R
@@ -8,20 +8,35 @@
 
 #Setup:
 require(tidyverse)
+setwd("C:/Users/bdh22/Dropbox/P8170S23/P8170-Lectures/SquirrelCodingSample/SquirrelNYC")
 
 
 #Data: 
-parks <- read.csv("~/SquirrelNYC/Data/park-data.csv")
-squirrel_data <- read.csv("~/SquirrelNYC/Data/squirrel-data.csv")
-
+parks <- read.csv("https://raw.githubusercontent.com/Benjamin-Huber/SquirrelNYC/main/Data/park-data.csv")
+squirrel_data <- read.csv("https://raw.githubusercontent.com/Benjamin-Huber/SquirrelNYC/main/Data/squirrel-data.csv")
 
 
 
 #Create Tree/Ground Variable
-
-
 #Create Eating/Foraging vs other activities variable
 
+squirrel_data <- squirrel_data %>% mutate(
+  above_ground = ifelse(str_detect(Location, "Above"), 1, 0),
+  eat_forag = case_when(
+    str_detect(Activities, regex("eat", ignore_case = TRUE)) ~ 1,
+    str_detect(Activities, regex("forag", ignore_case = TRUE)) ~ 1,
+    TRUE ~ 0
+  )
+)
+
+
 #Create Park Conditions Variable
+parks <- parks %>% mutate(
+  busy_park = ifelse(str_detect(Park.Conditions, regex("busy", ignore_case = TRUE)), 1, 0)
+)
+
 
 #Join Park Conditions variables with Squirrel data
+squirrel_analysis_data <- squirrel_data %>% left_join(parks %>% select(Park.ID, busy_park))
+
+write.csv(squirrel_analysis_data, "Data/squirrel_analysis_data.csv")
